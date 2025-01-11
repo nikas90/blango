34b2a00b70eee4dda4695d2c0b84c0dde46d0e10
Madalin NICA <madalin.nica@socgen.com>
2025-01-11 00:10:28 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
 
 import logging
 
